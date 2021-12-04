0311ae163bf580242f3fe842e344a69fe4710106
enzobenedetti <enzob210401@gmail.com>
2021-12-04 22:39:26 +0100

    Hold Button + Add Redo

--- Assets/Scripts/PointToCube.cs
+++ Assets/Scripts/PointToCube.cs
@@ -9,30 +9,34 @@ public class PointToCube : MonoBehaviour
 
     private Stack<Command> RedoCommands = new Stack<Command>();
 
-    private Color _colorSelected = Color.white;
+    private Color _colorSelected = Color.gray;
     public Slider redSlider;
     public Slider greenSlider;
     public Slider blueSlider;
     public Image previewColor;
 
+    private Transform _lastCubeColored;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
-                if (hit.transform.CompareTag("Cube"))
+                if (hit.transform.CompareTag("Cube") && hit.transform != _lastCubeColored)
                 {
                     Command command = new Coloring(_colorSelected,
                         hit.transform.GetComponent<MeshRenderer>().material.color, hit.transform);
                     command.Do();
                     Commands.Push(command);
+                    if (RedoCommands.Count > 0) RedoCommands.Clear();
+                    _lastCubeColored = hit.transform;
                 }
             }
         }
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButton("Fire2"))
         {
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
@@ -42,15 +46,26 @@ public class PointToCube : MonoBehaviour
                     Command command = new Erasing(hit.transform, hit.transform.GetComponent<MeshRenderer>().material.color);
                     command.Do();
                     Commands.Push(command);
+                    if (RedoCommands.Count > 0) RedoCommands.Clear();
                 }
             }
         }
 
         if (Input.GetButtonDown("Jump"))
         {
+            Debug.Log(Commands.Count);
             if (Commands.Count == 0) return;
             Command command = Commands.Pop();
             command.Undo();
+            RedoCommands.Push(command);
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            if (RedoCommands.Count == 0) return;
+            Command command = RedoCommands.Pop();
+            command.Do();
+            Commands.Push(command);
         }
     }
 
